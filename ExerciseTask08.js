e2dca6e95d692b1976ddd50dace60343d19abc32
Faiazur Rahman <faiazurrahman748@gmail.com>
2025-06-10 10:36:31 +0600

    test

--- ExerciseTask08.js
+++ ExerciseTask08.js
@@ -1,1 +1,11 @@
 //Task08: Write a program that checks if a given number N is a prime number.
+ //for
+ function prime(N){
+    for(i=2; i<=N-1;i++){
+        if(N%i!==0){
+                  
+        }
+    }
+
+ }
+ prime(9);
